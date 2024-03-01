34ee8190a5cdedb5b21e00bb5c2435b57d99bebb
lightbluepoppy <65863981+lightbluepoppy@users.noreply.github.com>
2024-03-01 01:35:12 +0100

    🐛fix(calendar): fix calendar overflow

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -4,16 +4,20 @@ import { DateSwitcher } from "src/components/calendar/DateSwitcher"
 import { CalendarViewTab } from "src/components/calendar/CalendarViewTab"
 import { EventSideEditor } from "./EventSideEditor"
 
+const CalendarHeader = () => (
+  <div className="fixed right-0 z-10 flex w-screen justify-end gap-1 bg-white">
+    <DateSwitcher />
+    <CalendarViewTab />
+  </div>
+)
+
 export const Calendar: React.FC<Props> = ({ children }) => {
   return (
     <div className="h-fit w-screen">
-      <div className="fixed z-10 flex w-screen justify-end gap-1 bg-white">
-        <DateSwitcher />
-        <CalendarViewTab />
-      </div>
+      <CalendarHeader />
       <div className="relative top-10 flex h-[1000px] flex-row">
         <HourLabels />
-        <div className="flex h-full w-fit overflow-x-scroll">{children}</div>
+        <div className="flex h-full w-fit">{children}</div>
         <EventSideEditor />
       </div>
     </div>
